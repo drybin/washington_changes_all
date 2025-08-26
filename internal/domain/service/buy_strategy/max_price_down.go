1c15d770228d8e743a599453fd5a85c0822148e0
Dmitry Rybin <d.rybin@office.ngs.ru>
2025-08-26 11:13:23 +0700

    Change max down price strategy calculation

--- internal/domain/service/buy_strategy/max_price_down.go
+++ internal/domain/service/buy_strategy/max_price_down.go
@@ -1,76 +1,78 @@
 package buy_strategy
 
 import (
-	"context"
-	"sort"
-
-	"github.com/drybin/washington_changes_all/internal/app/cli/config"
-	"github.com/drybin/washington_changes_all/internal/domain/model"
-	"github.com/drybin/washington_changes_all/internal/domain/repo"
-	"github.com/drybin/washington_changes_all/internal/domain/types/coin_name"
-	"github.com/drybin/washington_changes_all/pkg/wrap"
+    "context"
+    "sort"
+    
+    "github.com/drybin/washington_changes_all/internal/app/cli/config"
+    "github.com/drybin/washington_changes_all/internal/domain/model"
+    "github.com/drybin/washington_changes_all/internal/domain/repo"
+    "github.com/drybin/washington_changes_all/internal/domain/types/coin_name"
+    "github.com/drybin/washington_changes_all/pkg/wrap"
 )
 
 type MaxPriceDownStrategy struct {
-	Repo       repo.ICoinAvgPricesRepository
-	CoinConfig config.CoinConfig
+    Repo       repo.ICoinAvgPricesRepository
+    CoinConfig config.CoinConfig
 }
 
 type IMaxPriceDownStrategy interface {
-	Process(ctx context.Context, coins []config.Coin, marketsHistory *[]model.MarketInfo) ([]model.CoinPriceChange, error)
+    Process(ctx context.Context, coins []config.Coin, marketsHistory *[]model.MarketInfo) ([]model.CoinPriceChange, error)
 }
 
 func NewMaxPriceDownStrategy(
-	repo repo.ICoinAvgPricesRepository,
-	CoinConfig config.CoinConfig,
+    repo repo.ICoinAvgPricesRepository,
+    CoinConfig config.CoinConfig,
 ) IMaxPriceDownStrategy {
-	return &MaxPriceDownStrategy{Repo: repo, CoinConfig: CoinConfig}
+    return &MaxPriceDownStrategy{Repo: repo, CoinConfig: CoinConfig}
 }
 
 func (s *MaxPriceDownStrategy) Process(
-	ctx context.Context,
-	coins []config.Coin,
-	marketsHistory *[]model.MarketInfo,
+    ctx context.Context,
+    coins []config.Coin,
+    marketsHistory *[]model.MarketInfo,
 ) ([]model.CoinPriceChange, error) {
-
-	coinAvgPrices, err := s.Repo.GetList(ctx, MapConfigCoinArrayToModelArray(coins))
-	if err != nil {
-		return nil, wrap.Errorf("failed to get coins avg prices: %w", err)
-	}
-
-	result := calculateAvgPriceChange(coinAvgPrices, marketsHistory)
-
-	result = sortAthResultDesc(result)
-
-	return result, nil
+    
+    coinAvgPrices, err := s.Repo.GetList(ctx, MapConfigCoinArrayToModelArray(coins))
+    if err != nil {
+        return nil, wrap.Errorf("failed to get coins avg prices: %w", err)
+    }
+    
+    result := calculateAvgPriceChange(coinAvgPrices, marketsHistory)
+    
+    result = sortAthResultDesc(result)
+    
+    return result, nil
 }
 
 func calculateAvgPriceChange(coinResult *[]model.CoinPrice, marketsHistory *[]model.MarketInfo) []model.CoinPriceChange {
-	priceResult := make([]model.CoinPriceChange, 0, len(*coinResult))
-
-	for _, coin := range *coinResult {
-		for _, market := range *marketsHistory {
-			if market.Pair.CoinFirst.Name == coin.Coin.Name && market.Pair.CoinSecond.Name == coin_name.USDT {
-				percent := coin.Price / 100.0
-				priceChange := market.LastPrice / percent
-				priceResult = append(
-					priceResult,
-					model.CoinPriceChange{
-						Coin:        model.Coin{Name: coin.Coin.Name},
-						PriceChange: priceChange,
-						Ath:         0.0,
-					},
-				)
-			}
-		}
-	}
-	return priceResult
+    priceResult := make([]model.CoinPriceChange, 0, len(*coinResult))
+    
+    for _, coin := range *coinResult {
+        for _, market := range *marketsHistory {
+            if market.Pair.CoinFirst.Name == coin.Coin.Name && market.Pair.CoinSecond.Name == coin_name.USDT {
+                // percent := coin.Price / 100.0
+                // priceChange := market.LastPrice / percent
+                //26.08.2025 Experiment with price calc
+                priceChange := (coin.Price - market.LastPrice) / (coin.Price / 100.0)
+                priceResult = append(
+                    priceResult,
+                    model.CoinPriceChange{
+                        Coin:        model.Coin{Name: coin.Coin.Name},
+                        PriceChange: priceChange,
+                        Ath:         0.0,
+                    },
+                )
+            }
+        }
+    }
+    return priceResult
 }
 
 func sortAthResultDesc(athResult []model.CoinPriceChange) []model.CoinPriceChange {
-	sort.Slice(athResult, func(i, j int) bool {
-		return athResult[i].PriceChange > athResult[j].PriceChange
-	})
-
-	return athResult
+    sort.Slice(athResult, func(i, j int) bool {
+        return athResult[i].PriceChange > athResult[j].PriceChange
+    })
+    
+    return athResult
 }
